7bd404df343590d7daee26c9f1bd14e90fc40b6f
KDH5706 <rpaehdgns12@naver.com>
2022-05-30 17:41:29 +0900

    220530 월요일 wpf 시작

---
+++ day01/WpfBikeShop/Car.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace WpfBikeShop
+{
+    public class Car
+    {
+        public double Speed { get; set; }
+        public Color Color { get; set; }
+        public Car()
+        {
+        }
+    }
+}
