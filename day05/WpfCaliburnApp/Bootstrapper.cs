9e4e53fe54de10c1c2f935cbde787b5906315d30
KDH5706 <rpaehdgns12@naver.com>
2022-06-07 17:44:43 +0900

    220607 커밋

---
+++ day05/WpfCaliburnApp/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Caliburn.Micro;
+using System.Windows;
+using WpfCaliburnApp.ViewModels;
+
+namespace WpfCaliburnApp
+{
+    /// <summary>
+    /// 시작 윈도우를 지정하기 위해 사용! ^^7
+    /// </summary>
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //base.OnStartup(sender, e);
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ day05/WpfMvvmApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace WpfMvvmApp.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            //if(PropertyChanged != null)
+            //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
